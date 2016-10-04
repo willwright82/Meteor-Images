dce0b8fb2800641a7bb4858057318c51a2d4041c
Will Wright <will@clazzoo.com>
2016-10-04 13:33:50 +0100

    Lesson 19

--- client/main.jsx
+++ client/main.jsx
@@ -3,6 +3,7 @@
 // Import React library
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import ImageList from './components/image_list';
 
 // Create a component
@@ -17,4 +18,6 @@ const App = () => {
 // Render this component on the screen
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.container'));
+  axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+  .then(response => console.log(response));
 });
