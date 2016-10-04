11efd47c2f80e567e367e1af1ccb5b5761890e82
Will Wright <will@clazzoo.com>
2016-10-04 13:41:58 +0100

    Lesson 21

--- client/main.jsx
+++ client/main.jsx
@@ -1,23 +1,25 @@
 // Any code here will immediately run
 
 // Import React library
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import ImageList from './components/image_list';
 
 // Create a component
-const App = () => {
-  return (
+class App extends Component {
+  render() {
+    return (
     <div>
       <ImageList />
     </div>
-  );
+    );
+  }
 };
 
 // Render this component on the screen
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.container'));
   axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
-  .then(response => console.log(response));
+    .then(response => console.log(response));
 });
