7c9c092ad9161ea46e2ff3d5503d4138b1d3d1fa
Will Wright <will@clazzoo.com>
2016-10-04 13:29:30 +0100

    Lesson 18

--- client/components/image_list.jsx
+++ client/components/image_list.jsx
@@ -14,7 +14,7 @@ const IMAGES = [
 // Create our component
 const ImageList = () => {
   const RenderedImages = IMAGES.map(function(image) {
-    return <ImageDetail image={image} />
+    return <ImageDetail key={image.title} image={image} />
   });
   return (
     <ul className="media-list list-group">
